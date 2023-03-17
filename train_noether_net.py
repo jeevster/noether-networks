2f0a3255b9ce83121d48105f2509e9ad9e0622ee
Nithin Chalapathi <nithin.ch10@gmail.com>
2023-03-16 23:45:09 -0700

    fixes for summary

--- train_noether_net.py
+++ train_noether_net.py
@@ -339,12 +339,17 @@ for trial_num in range(opt.num_trials):
             print(f'embedding ckpt path given but no embedding found...')
     
     svg_model.apply(lambda t: t.cuda())
+    model_kwargs = {
+        "opt":opt, 
+        "mode":'eval',
+        "i":opt.n_past+2,
+    }
     print('Eval summary')
-    summary(svg_model, input_size=(opt.n_past, opt.channels, opt.image_width, opt.image_width), device=torch.device("cuda"), opt=opt, mode='eval', i=opt.n_past+2)
+    summary(svg_model, input_size=(opt.n_past, opt.channels, opt.image_width, opt.image_width), device=torch.device("cuda"), mode='eval', kwargs=model_kwargs)
     print('Train summary')
-    summary(svg_model, input_size=(opt.n_past, opt.channels, opt.image_width, opt.image_width), device=torch.device("cuda"), opt=opt, mode='train', i=opt.n_past+2)
+    summary(svg_model, input_size=(opt.n_past, opt.channels, opt.image_width, opt.image_width), device=torch.device("cuda"), mode='train', kwargs=model_kwargs)
     print('Emb summary')
-    summary(svg_model, input_size=(opt.n_past, opt.channels, opt.image_width, opt.image_width), device=torch.device("cuda"), opt=opt, mode='emb', i=opt.n_past+2)
+    summary(svg_model, input_size=(opt.n_past, opt.channels, opt.image_width, opt.image_width), device=torch.device("cuda"), kwargs=model_kwargs)
     # For comparing later
     old_state_dict = copy.deepcopy(svg_model.state_dict())
     if opt.baseline:
