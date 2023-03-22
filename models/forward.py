7fe9350146adaefd85c0536fe6eb7dbebecdab0a
Nithin Chalapathi <nithin.ch10@gmail.com>
2023-03-22 12:55:54 -0700

    fwd

--- models/forward.py
+++ models/forward.py
@@ -65,6 +65,11 @@ def inner_crit(fmodel, gen_seq, mode='mse', num_emb_frames=1, compare_to='prev',
         elif compare_to == 'zero_and_prev':
             pairwise_inner_losses = torch.stack([F.mse_loss(embs[t-1], embs[t], reduction='none') for t in range(
                 1, len(embs))] + [F.mse_loss(embs[0], embs[t], reduction='none') for t in range(1, len(embs))]).mean(dim=2)
+        elif compare_to == 'pde_zero':
+            import pdb
+            pdb.set_trace()
+            pairwise_inner_losses = torch.stack(
+                [pairwise_inner_losses[t-1] for t in range(1, len(embs))])
         else:
             raise ValueError('must choose prev or zero or zero_and_prev')
     elif mode == 'cosine':
