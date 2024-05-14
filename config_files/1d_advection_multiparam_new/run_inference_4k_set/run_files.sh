272cec56cc8f02425e15c2bf547a19b9e7586a3c
divyam123-EECS-Physics <divyam123@berkeley.edu>
2024-05-14 07:24:48 -0700

    05-14

--- config_files/1d_advection_multiparam_new/run_inference_4k_set/run_files.sh
+++ config_files/1d_advection_multiparam_new/run_inference_4k_set/run_files.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=6
+CUDA_VISIBLE_DEVICES=2
 DIR=/home/divyam123/noether_work_cpy/noether-networks/noether-networks/config_files/1d_advection_multiparam_new/run_inference_4k_set
-for FILE in $DIR/new_residual_experiments/*
+for FILE in $DIR/run_all_inference/*
 do
     bash $FILE
 done;
--- config_files/1d_burgers_multiparam_new/run_inference_4k_set/run_files.sh
+++ config_files/1d_burgers_multiparam_new/run_inference_4k_set/run_files.sh
@@ -1,5 +1,5 @@
 DIR=/home/divyam123/noether_work_cpy/noether-networks/noether-networks/config_files/1d_burgers_multiparam_new/run_inference_4k_set
-for FILE in $DIR/new_residual_experiments/*
+for FILE in $DIR/run_all_inference/*
 do
     bash $FILE
 done;
